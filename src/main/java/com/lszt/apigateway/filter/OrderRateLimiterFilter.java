e64e783713d1bf45cd849c4aa553b2ca8f2b90aa
cloud <451597529@qq.com>
2018-09-15 20:28:37 +0800

    2018/9/15修改

--- src/main/java/com/lszt/apigateway/filter/OrderRateLimiterFilter.java
+++ src/main/java/com/lszt/apigateway/filter/OrderRateLimiterFilter.java
@@ -10,26 +10,45 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/***
+ * 网关限流
+ */
 @Component
 public class OrderRateLimiterFilter extends ZuulFilter {
-    //每秒产生1000个令牌
-    private static final RateLimiter RATE_LIMITER = RateLimiter.create(500);
+    //每秒产生1000个令牌 如果集群1000/集群个数
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(1000);
 
+    /**
+     * 前置过滤器
+     * pre：可以在请求被路由之前调用
+     * route：在路由请求时候被调用
+     * post：在route和error过滤器之后被调用
+     * error：处理请求时发生错误时被调用
+     * @return
+     */
     @Override
     public String filterType() {
         return PRE_TYPE;
     }
 
+    /**
+     * 优先级为0，数字越大，优先级越低
+     * @return
+     */
     @Override
     public int filterOrder() {
         return -4;
     }
 
+    /**
+     * 是否执行该过滤器，此处为true，说明需要过滤
+     * @return
+     */
     @Override
     public boolean shouldFilter() {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
-        //只对订单接口做限流
         if ("/apigateway/order/api/order/save".equalsIgnoreCase(request.getRequestURI())) {
             return true;
         }
